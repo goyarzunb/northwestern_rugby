ec588ff1af317f7ff4c22dfe72dc98afd7278a9a
Gabriela Oyarzun Batlle <gabrielabatlle2025@u.northwestern.edu>
2024-08-01 18:31:27 -0500

    Prepare for deployment

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
 import './main.css';
 
 const Slideshow = () => {
+  const [caption, setCaption] = useState('');
+
+  const handleMouseEnter = (captionText) => {
+    setCaption(captionText);
+  };
+
+  const handleMouseLeave = () => {
+    setCaption('');
+  };
+
   return (
     <Carousel>
-      <Carousel.Item>
+      <Carousel.Item
+        onMouseEnter={() => handleMouseEnter('Group picture of after both teams won first place in Lake Side 7s!')}
+        onMouseLeave={handleMouseLeave}
+      >
         <img
           className="d-block w-100"
-          src={`${process.env.PUBLIC_URL}/Pictures/group_picture.jpeg`}
-          alt="Men and Womens' team after winning first place at Lake Side 7s"
+          src="Pictures/group_picture.jpeg"
+          alt="Group Picture"
         />
-        <Carousel.Caption>
-          <h3>Group picture after both teams won first place in Lake Side 7s!</h3>
-        </Carousel.Caption>
+        {caption && (
+          <Carousel.Caption>
+            <h3>{caption}</h3>
+          </Carousel.Caption>
+        )}
       </Carousel.Item>
 
-      <Carousel.Item>
+      <Carousel.Item
+        onMouseEnter={() => handleMouseEnter("Women's Team")}
+        onMouseLeave={handleMouseLeave}
+      >
         <img
           className="d-block w-100"
-          src={`${process.env.PUBLIC_URL}/Pictures/all-min.jpeg`}
-          alt="Women's Team Group Picture"
+          src="Pictures/all-min.jpeg"
+          alt="Women's Team"
         />
-        <Carousel.Caption>
-          <h3>Women's Team</h3>
-        </Carousel.Caption>
+        {caption && (
+          <Carousel.Caption>
+            <h3>{caption}</h3>
+          </Carousel.Caption>
+        )}
       </Carousel.Item>
     </Carousel>
   );
 };
 
 export default Slideshow;
+
+
